64f675e578a9be6b344a90f58ceafbe3ef30f146
MikouZonata <kevin.vanas@student.hku.nl>
2019-08-25 22:17:17 +0200

    clean for new gitignore

--- src/Opdracht2/Strategies/Strategy.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include"../Cell.h"
-
-namespace NSAssignmentTwo {
-	class Strategy {
-	public:
-		virtual bool checkCell(Cell cell, int aliveNeighbours) const = 0;
-	};
-}
--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "ofMain.h"
-#include "ofApp.h"
-
-//========================================================================
-int main( ){
-	ofSetupOpenGL(876,876,OF_WINDOW);
-	ofRunApp(new ofApp());
-}
